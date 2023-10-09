7a7a486d015f8fc70ed63e032f3b1d2e097c6d2c
Puran Vishwakarma <130432123+puran66@users.noreply.github.com>
2023-10-09 15:25:57 +0530

    Add files via upload

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -1,22 +1,31 @@
-let FullName = document.querySelector("#FullName");
+let Form_1 = document.getElementsByClassName("form-1");
 let Address = document.querySelector("#Address");
 let Age = document.querySelector("#Age");
 let Job = document.querySelector("#Job");
 let Phone = document.querySelector("#Phone");
 let Country = document.querySelector("#Country");
-let Sumbit = document.querySelector("#Sumbit");
+let Submit = document.querySelector("#Sumbit1");
 
+try {
+    Submit.addEventListener('click', () => {
+        if (FullName.value && Address.value && Age.value > 17 && Job.value && Phone.value && Country.value) {
+            let Form = {
+                Name: FullName.value,
+                Address: Address.value,
+                Age: Age.value,
+                Job: Job.value,
+                Contact: Phone.value,
+                Country: Country.value
+            };
 
+            localStorage.setItem("details", JSON.stringify(Form));
 
+            window.location.reload();
+        } else {
+            throw "Your Age Must be Above 18";
+        }
 
-StoreChcek=()=>{
-    if(FullName && Address && Age>17 && Job && Phone && Country){
-        
-    }
-    else{
-        throw error("")
-    }
+    });
+} catch (error) {
+    prompt(error);
 }
-
-Sumbit.addEventListener('click',StoreChcek)
-
